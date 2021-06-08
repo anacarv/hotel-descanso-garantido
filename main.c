b2ff20794d1dc0fdda5e9bae468f985329175066
Ana Carolina <83416353+anacarv@users.noreply.github.com>
2021-06-08 20:54:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int codigoCliente()
 {
     int codigoCliente=0;
     codigoCliente++;
-    printf("\nC”DIGO %i\n",codigoCliente);
+    printf("\nC√ìDIGO %i\n",codigoCliente);
     return (codigoCliente);
 }
 int nomeCliente()
@@ -40,13 +40,13 @@ int nomeCliente()
     fflush(stdin);
     gets(nomeCliente);
 
-    //O nome deve ser uma letra do alfabeto ou um caractere espaÁo, enquanto ele n„o for assim, n„o È possÌvel prosseguir
+    //O nome deve ser uma letra do alfabeto ou um caractere espa√ßo, enquanto ele n√£o for assim, n√£o √© poss√≠vel prosseguir
 
     for(x=0; x<strlen(nomeCliente); x++)
     {
         while(!((nomeCliente[x]>='a' && nomeCliente[x]<='z') || (nomeCliente[x]>='A' && nomeCliente[x]<='Z') || (nomeCliente[x]==' ')))
         {
-            printf("\nPor favor insira um nome v·lido.\n");
+            printf("\nPor favor insira um nome v√°lido.\n");
             printf("\nCliente:\n");
             fflush(stdin);
             gets(nomeCliente);
@@ -59,7 +59,7 @@ int enderecoCliente()
 {
     int numeroEndereco=0, apartamentoEndereco=0, tipoDeEndereco=0;
     char bairroEndereco[36], ruaEndereco[51];
-    printf("\nCADASTRO DE ENDERE«O\n");
+    printf("\nCADASTRO DE ENDERE√áO\n");
 
     //Apartamento ou casa
     printf("\n1- Apartamento\n2-Casa\n");
@@ -75,13 +75,13 @@ int enderecoCliente()
         printf("Bairro\t");
         fflush(stdin);
         gets(bairroEndereco);
-        //N˙mero do endereÁo do cliente
-        printf("N˙mero\t");
+        //N√∫mero do endere√ßo do cliente
+        printf("N√∫mero\t");
         scanf("%d",&numeroEndereco);
-        //N˙mero do apartamento
-        printf("N˙mero do apartamento\t");
+        //N√∫mero do apartamento
+        printf("N√∫mero do apartamento\t");
         scanf("%d",&apartamentoEndereco);
-        printf("O endereÁo rua %s, n˙mero %i, apto %i, no bairro %s, foi cadastrado com sucesso!\n",ruaEndereco,numeroEndereco,apartamentoEndereco,bairroEndereco);
+        printf("O endere√ßo rua %s, n√∫mero %i, apto %i, no bairro %s, foi cadastrado com sucesso!\n",ruaEndereco,numeroEndereco,apartamentoEndereco,bairroEndereco);
         return(ruaEndereco,numeroEndereco,apartamentoEndereco,bairroEndereco);
     }
     else
@@ -97,16 +97,16 @@ int enderecoCliente()
             printf("Bairro\t");
             fflush(stdin);
             gets(bairroEndereco);
-            //N˙mero do endereÁo do cliente
-            printf("N˙mero\t");
+            //N√∫mero do endere√ßo do cliente
+            printf("N√∫mero\t");
             scanf("%d",&numeroEndereco);
-            printf("O endereÁo rua %s, n˙mero %i, no bairro %s, foi cadastrado com sucesso!\n",ruaEndereco,numeroEndereco,bairroEndereco);
+            printf("O endere√ßo rua %s, n√∫mero %i, no bairro %s, foi cadastrado com sucesso!\n",ruaEndereco,numeroEndereco,bairroEndereco);
             return(ruaEndereco,numeroEndereco,bairroEndereco);
         }
         //Entrada incorreta
         else
         {
-            printf("Por favor insire um n˙mero v·lido (1 ou 2)\n");
+            printf("Por favor insire um n√∫mero v√°lido (1 ou 2)\n");
         }
     }
 }
@@ -124,7 +124,7 @@ int telefoneCliente()
     while(dddClienteFixo>100 || dddClienteFixo<=10);
     do
     {
-        printf("N˙mero de telefone fixo: \t");
+        printf("N√∫mero de telefone fixo: \t");
         scanf("%d",&fixoCliente);
     }
     while(fixoCliente<10000000 || fixoCliente>99999999);
@@ -138,19 +138,12 @@ int telefoneCliente()
     while(dddClienteCelular>100 || dddClienteCelular<=10);
     do
     {
-        printf("N˙mero: \t");
+        printf("N√∫mero: \t");
         scanf("%d",&celularCliente);
-        //ver se o primeiro dÌgito È 9
+        //ver se o primeiro d√≠gito √© 9
         numInteiro=celularCliente/100000000;
     }
     while(celularCliente<100000000 || celularCliente>999999999 || numInteiro!=9);
     printf("O telefone fixo %i-%i, e o telefone celular %i-%i foram cadastrados com sucesso!\n",dddClienteFixo,fixoCliente,dddClienteCelular,celularCliente);
     return(dddClienteFixo,fixoCliente,dddClienteCelular,celularCliente);
 }
-
-
-
-
-
-
-
